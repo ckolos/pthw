9b8efebf5026668b908fd144e9c344647e8bb9bd
Chris Kolosiwsky <ckolos-=at=-gmail-=dot=-com>
2015-04-22 19:59:17 -0500

    ex4 study drill 3

--- ex4/ex4.py
+++ ex4/ex4.py
@@ -1,10 +1,25 @@
+# number of cars available
 cars = 100
+
+# how many people can we cram in one car
 space_in_a_car = 4.0
+
+# how many drivers do we have
 drivers = 30
+
+# How many people are riding not driving
 passengers = 90
+
+# how many cars don't have drivers
 cars_not_driven = cars - drivers
+
+# each driver can only drive 1 car
 cars_driven = drivers
+
+# how many people can carpool?
 carpool_capacity = cars_driven * space_in_a_car
+
+# how many people will be riding in each of the available cars?
 average_passengers_per_car = passengers / cars_driven
 
 print "There are", cars, "cars available."
