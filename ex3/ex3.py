1f932b56fd5e061b011f2980792636513ecd9d98
Chris Kolosiwsky <ckolos-=at=-gmail-=dot=-com>
2015-04-22 19:45:33 -0500

    first three exercises

---
+++ ex3/ex3.py
@@ -0,0 +1,31 @@
+# prints  "I will count my chickens:"
+print "I will count my chickens:"
+
+# calculates 25 + 30/6
+print "Hens", 25 + 30/6
+
+# Calculates 100 - ((25 * 3) % 4) - % is modulo
+print "Roosters", 100 - 25 * 3 % 4
+
+# Calculates 3+2+1-5+ (4 % 2) - (1/4) +6
+# 1 + 0 + 0 + 6
+print "Now I will count the eggs"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# prints false b/c 5 is not < -2
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False"
+# print True
+print "Is it greater?", 5 > -2
+
+# print True
+print "Is it greater or equal?", 5 >= -2
+
+# Print False
+print "Is it less than or equal?", 5 <= -2
---
+++ ex3/ex3_pi.py
@@ -0,0 +1,5 @@
+# Calculates the area of a circle
+pi = 3.14
+radius = 9.0
+
+print "The area of a circle with a radius of", radius, "is", pi * radius * radius
