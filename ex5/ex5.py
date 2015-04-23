b7ca8ab1d8df31c485e4a7b5d6cf1125ebbce9ba
Chris Kolosiwsky <ckolos-=at=-gmail-=dot=-com>
2015-04-22 21:24:07 -0500

    ex5 study drill 3

--- ex5/ex5.py
+++ ex5/ex5.py
@@ -14,4 +14,9 @@ print "He's got %s eyes and %s hair" % (eyes, hair)
 print "His teet are usually %s, depending on the coffee" % teeth
 
 print "If I add %d, %d, and %d, I get %d" % ( age, height, weight, age + height + weight)
+total = age + height + weight
 print "Testing out the %%r variable: %r" % "foo%1"
+
+print "testing out the various python formatting characters"
+print "Here is the number %d as a hex, lower case, using %%x: %x" % (total,total)
+print "Here is the number %d as a hex, upper case, using %%X: %X" % (total,total)
