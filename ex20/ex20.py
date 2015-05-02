725ddc0d6f02a8437a17fbc316e469e20f4cc6c9
Chris Kolosiwsky <ckolos-=at=-gmail-=dot=-com>
2015-05-02 09:41:25 -0500

    trace current_line v. line_count

--- ex20/ex20.py
+++ ex20/ex20.py
@@ -9,6 +9,7 @@ def rewind(f):
   f.seek(0)
 
 def print_a_line(line_count, f):
+  print "Line count is: %d" % line_count
   print line_count, f.readline()
 
 current_file = open(input_file)
@@ -22,10 +23,13 @@ rewind(current_file)
 print "Let's print three lines:"
 
 current_line = 1
+print "Current line is: %d" % current_line
 print_a_line(current_line, current_file)
 
 current_line = current_line + 1
+print "Current line is: %d" % current_line
 print_a_line(current_line, current_file)
 
 current_line = current_line + 1
+print "Current line is: %d" % current_line
 print_a_line(current_line, current_file)
