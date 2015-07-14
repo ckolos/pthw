67600f1120c4ea050b69dffa8fe981182404e3d8
Chris Kolosiwsky <ckolos@gmail.com>
2015-07-13 19:12:35 -0500

    ex33 q study drill 1

--- ex33/3x33.py
+++ ex33/3x33.py
@@ -1,13 +1,19 @@
-i = 0
+from sys import argv
+i=0
+iterator=int(argv[1])
+print argv
 numbers = []
-while i < 6:
-  print "At the top i is %d" % i
-  numbers.append(i)
 
-  i = i + 1
-  print "Numbers now: ", numbers
-  print "At the bottom i in %d" % i
+def while_loop(iterator):
+  global i
+  while i < iterator:
+    print "At the top i is %d" % iterator
+    numbers.append(i)
+    i+=1
+    print "Numbers now: ", numbers
+    print "At the bottom i in %d" % i
 
+while_loop(iterator)
 print "The numbers: "
 for num in numbers:
   print num
