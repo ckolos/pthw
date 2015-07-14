ec16101a0104cb7b976bbc5dc6f97971265b00eb
Chris Kolosiwsky <ckolos@gmail.com>
2015-07-13 19:24:25 -0500

    study q 3

--- ex33/3x33.py
+++ ex33/3x33.py
@@ -1,19 +1,21 @@
 from sys import argv
 i=0
-iterator=int(argv[1])
-print argv
+script = argv[0]
+iterator = int(argv[1])
+stepper = int(argv[2])
+
 numbers = []
 
-def while_loop(iterator):
+def while_loop(iterator, stepper):
   global i
   while i < iterator:
     print "At the top i is %d" % iterator
     numbers.append(i)
-    i+=1
+    i+=stepper
     print "Numbers now: ", numbers
     print "At the bottom i in %d" % i
 
-while_loop(iterator)
+while_loop(iterator, stepper)
 print "The numbers: "
 for num in numbers:
   print num
